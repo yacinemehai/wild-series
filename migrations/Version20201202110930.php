a5fac7236003ecdefb379b7f13c0e8797e334d0e
yacinemehai <yacine.mehai@gmail.com>
2020-12-03 10:28:42 +0100

    quest 9

---
+++ migrations/Version20201202110930.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201202110930 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE episode');
+        $this->addSql('ALTER TABLE season ADD program_id INT DEFAULT NULL, DROP program');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT FK_F0E45BA93EB8070A FOREIGN KEY (program_id) REFERENCES program (id)');
+        $this->addSql('CREATE INDEX IDX_F0E45BA93EB8070A ON season (program_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE episode (id INT AUTO_INCREMENT NOT NULL, season INT NOT NULL, title VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, number INT NOT NULL, synopsis LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE season DROP FOREIGN KEY FK_F0E45BA93EB8070A');
+        $this->addSql('DROP INDEX IDX_F0E45BA93EB8070A ON season');
+        $this->addSql('ALTER TABLE season ADD program INT NOT NULL, DROP program_id');
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,7 +2,9 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
+use App\Entity\Episode;
 use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
     {
         /**
-         * @Route ("/show/{id<^[0-9]+$>}", name="show")
+         * @Route ("/{id<^[0-9]+$>}", name="show")
          * @return Response
          */
         public function show($id): Response
@@ -22,6 +24,11 @@ class ProgramController extends AbstractController
                 ->getRepository(Program::class)
                 ->findOneBy(['id' => $id]);
 
+            $seasons = $this->getDoctrine()
+                ->getRepository(Season::class)
+                ->findBy(['program' => $program]);
+
+
             if (!$program) {
                 throw $this->createNotFoundException(
                     'No program with id : ' . $id . ' found in program\'s table.'
@@ -29,7 +36,30 @@ class ProgramController extends AbstractController
             }
             return $this->render('program/show.html.twig', [
                 'program' => $program,
+                'seasons' => $seasons,
             ]);
+        }
+
+         /**
+        * @Route ("/{programId}/seasons/{seasonId}", name="season_show")
+        * @return Response
+        */
+        public function showSeason(int $programId, int $seasonId) : Response
+        {
+            $programId = $this->getDoctrine()
+                ->getRepository(Program::class)
+                ->findOneBy(['id' => $programId]);
+
+            $seasonId = $this->getDoctrine()
+                ->getRepository(Season::class)
+                ->findOneBy(['id' => $seasonId]);
+
+            $episodes = $this->getDoctrine()
+                ->getRepository(Episode::class)
+                ->findBy(['season' => $seasonId]);
+
+            return $this->render('/program/season_show.html.twig',
+                ['program' => $programId, 'season' => $seasonId, 'episodes' => $episodes]);
         }
 
         /**
