c89e2beba54e3313548f33e363ba355580b94012
yacinemehai <yacine.mehai@gmail.com>
2020-12-09 18:16:25 +0100

    quest15

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -20,7 +22,13 @@ class ProgramType extends AbstractType
             ->add('country')
             ->add('year')
             ->add('category_id', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
